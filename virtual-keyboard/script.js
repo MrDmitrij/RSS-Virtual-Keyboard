9caf49b88aeb44e0cbf0b0571a46c1b5ca5fdc96
MrDmitrij <mr.dima.markevich@mail.ru>
2023-05-03 16:39:00 +0300

    feat: add english layout buttons

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -51,4 +51,82 @@ function addTextSwitchingLanguage() {
 
 addTextSwitchingLanguage();
 
+let en;
+const KEYBOARD_LINES = [['Backquote',
+    'Digit1',
+    'Digit2',
+    'Digit3',
+    'Digit4',
+    'Digit5',
+    'Digit6',
+    'Digit7',
+    'Digit8',
+    'Digit9',
+    'Digit0',
+    'Minus',
+    'Equal',
+    'Backspace'], ['Tab',
+    'KeyQ',
+    'KeyW',
+    'KeyE',
+    'KeyR',
+    'KeyT',
+    'KeyY',
+    'KeyU',
+    'KeyI',
+    'KeyO',
+    'KeyP',
+    'BracketLeft',
+    'BracketRight',
+    'Backslash',
+    'Delete'], ['CapsLock',
+    'KeyA',
+    'KeyS',
+    'KeyD',
+    'KeyF',
+    'KeyG',
+    'KeyH',
+    'KeyJ',
+    'KeyK',
+    'KeyL',
+    'Semicolon',
+    'Quote',
+    'Enter'], ['ShiftLeft',
+    'KeyZ',
+    'KeyX',
+    'KeyC',
+    'KeyV',
+    'KeyB',
+    'KeyN',
+    'KeyM',
+    'Comma',
+    'Period',
+    'Slash',
+    'ArrowUp',
+    'ShiftRight'], ['ControlLeft',
+    'MetaLeft',
+    'AltLeft',
+    'Space',
+    'AltRight',
+    'ArrowLeft',
+    'ArrowDown',
+    'ArrowRight',
+    'ControlRight']];
+  const rowsContainer = [];
+
+function generateButtons() {
+  for (let i = 0; i < KEYBOARD_LINES.length; i++) {
+    rowsContainer.push(new BuildElements('div', keyBoardContainer, ['row']).getElement());
+    for (let j = 0; j < KEYBOARD_LINES[i].length; j++) {
+      const EN_DIV = new BuildElements('div', rowsContainer[i], [KEYBOARD_LINES[i][j], 'button']).getElement();
+      EN_DIV.innerText = en[KEYBOARD_LINES[i][j]].shiftDown;
+    }
+  }
+}
+
+fetch('json/en.json').then((response) => response.json()).then((json) => {
+  en = json;
+  generateButtons();
+});
+
 alert('Здравствуйте, просьба по возможности проверить работу позже, за вторник - среду(до вечера) сделаю, спасибо за понимание!');
