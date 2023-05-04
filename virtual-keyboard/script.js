dc8e7109c60ea50b4108acd3d10fd76da1903185
MrDmitrij <mr.dima.markevich@mail.ru>
2023-05-04 13:55:31 +0300

    feat: implement switching between en and ru layouts

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -52,6 +52,8 @@ function addTextSwitchingLanguage() {
 addTextSwitchingLanguage();
 
 let en;
+let ru;
+let currentLocale = 'ru';
 const KEYBOARD_LINES = [['Backquote',
     'Digit1',
     'Digit2',
@@ -112,21 +114,116 @@ const KEYBOARD_LINES = [['Backquote',
     'ArrowDown',
     'ArrowRight',
     'ControlRight']];
-  const rowsContainer = [];
+const rowsContainer = [];
+let ctrlLeftPressed = false;
+let altLeftPressed = false;
+let ctrlRightPressed = false;
+let altRightPressed = false;
+const auxKeys = [];
+const LANGUAGES = ['ru', 'en'];
 
 function generateButtons() {
   for (let i = 0; i < KEYBOARD_LINES.length; i++) {
     rowsContainer.push(new BuildElements('div', keyBoardContainer, ['row']).getElement());
     for (let j = 0; j < KEYBOARD_LINES[i].length; j++) {
-      const EN_DIV = new BuildElements('div', rowsContainer[i], [KEYBOARD_LINES[i][j], 'button']).getElement();
+      const KEY = new BuildElements('div', rowsContainer[i], [KEYBOARD_LINES[i][j], 'key', 'hover']).getElement();
+      const EN_DIV = new BuildElements('div', KEY, ['en']).getElement();
+      const RU_DIV = new BuildElements('div', KEY, ['ru']).getElement();
       EN_DIV.innerText = en[KEYBOARD_LINES[i][j]].shiftDown;
+      RU_DIV.innerText = ru[KEYBOARD_LINES[i][j]].shiftDown;
+      if (ru[KEYBOARD_LINES[i][j]].shiftUp === ru[KEYBOARD_LINES[i][j]].shiftDown) {
+        auxKeys.push(KEYBOARD_LINES[i][j]);
+      }
     }
   }
 }
 
+function disableElementsByClass(...classList) {
+  if (classList) {
+    classList.forEach((cl) => {
+      const elements = document.querySelectorAll(`.${cl}`);
+      elements.forEach((el) => el.classList.add('hidden'));
+    });
+  }
+}
+
+function enableElemenstByClass(...classList) {
+  if (classList) {
+    classList.forEach((cl) => {
+      const elements = document.querySelectorAll(`.${cl}`);
+      elements.forEach((el) => {
+        el.classList.remove('hidden');
+      });
+    });
+  }
+}
+
+function updateKeyboard() {
+  if (currentLocale === 'ru') {
+    enableElemenstByClass('ru');
+    disableElementsByClass('en');
+  } else if (currentLocale === 'en') {
+    enableElemenstByClass('en');
+    disableElementsByClass('ru');
+  }
+}
+
+function checkLanguageSwitch() {
+  if (ctrlLeftPressed && altLeftPressed) {
+    currentLocale = LANGUAGES.indexOf(currentLocale) ? 'ru' : 'en';
+    localStorage.setItem('language', currentLocale);
+    setTimeout(() => {
+      ctrlLeftPressed = false;
+      altLeftPressed = false;
+    }, 300);
+  }
+}
+
+function constrolsPressed(key) {
+  if (key === 'ControlLeft') {
+    ctrlLeftPressed = !ctrlLeftPressed;
+    checkLanguageSwitch();
+  } else if (key === 'ControlRight') {
+    ctrlRightPressed = !ctrlRightPressed;
+  } else if (key === 'AltLeft') {
+    altLeftPressed = !altLeftPressed;
+    checkLanguageSwitch();
+  } else if (key === 'AltRight') {
+    altRightPressed = !altRightPressed;
+  }
+  updateKeyboard();
+}
+
+function specialActionKeysPressed(key) {
+  switch (key) {
+    case 'ControlLeft':
+    case 'ControlRight':
+    case 'AltLeft':
+    case 'AltRight':
+      constrolsPressed(key);
+      break;
+    default:
+      break;
+  }
+}
+
+function pressRealKeyboard(e) {
+  e.preventDefault();
+  if (auxKeys.includes(e.code)) {
+    specialActionKeysPressed(e.code);
+  }
+} 
+
 fetch('json/en.json').then((response) => response.json()).then((json) => {
   en = json;
-  generateButtons();
+  fetch('json/ru.json').then((response) => response.json()).then((json2) => {
+    ru = json2;
+    generateButtons();
+    updateKeyboard();
+  });
 });
+
+document.addEventListener('keydown', pressRealKeyboard);
+document.addEventListener('keyup', pressRealKeyboard);
 
 alert('Здравствуйте, просьба по возможности проверить работу позже, за вторник - среду(до вечера) сделаю, спасибо за понимание!');
